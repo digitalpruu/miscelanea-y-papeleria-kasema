f8e121c05f512a0f371a4a63466eaac92fb79840
kevinpicon01 <digitalpruu@gmail.com>
2024-10-01 16:32:19 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,9 +1,9 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Miscelánea y Papelería Kasema";
+const email = "nancyceballes1974@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en los productos de ${title} y me gustaría recibir más información. ¡Espero su respuesta!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "3104575544";
 const textos = {
     index: {
         title: title,
@@ -12,25 +12,24 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "*Miscelánea y Papelería Kasema* te ofrece una amplia gama de productos, desde útiles escolares y papelería hasta cosméticos y perfumería. Somos tu tienda de confianza para encontrar todo lo que necesitas en un solo lugar.",
+        description2: "Nuestro objetivo es ofrecerte productos de calidad al mejor precio, con un servicio cercano y personalizado. Ya sea que necesites material escolar, artículos de oficina o productos para el hogar, en *Kasema* estamos para atender todas tus necesidades. Visítanos y descubre por qué somos la tienda preferida en la comunidad. ¡En *Kasema*, siempre encontrarás lo que buscas!",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Miscelánea y Papelería Kasema",
+            p2: "Todo lo que necesitas, en un solo lugar.",
         },
-        address: "Dirección del emprendimiento",
+        address: "",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
+        facebook: "https://www.facebook.com/profile.php?id=61565502966209&mibextid=ZbWKwL",
         instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
 export default textos;
